6552ec35b3f2509475f7a5ca65cf74adb5a9659a
Abhishek Tripathi <ronalabhishek@gmail.com>
2025-11-19 03:02:53 +0530

    #Minor changes

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,14 @@ require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",   
+    credentials: true,                 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { FileProvider } from './context/FileContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <FileProvider>
+        <App />
+      </FileProvider>
+    </AuthProvider>
   </StrictMode>,
 )
